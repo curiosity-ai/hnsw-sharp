7ddb603d877f899d61dfc758af579157420fddf6
rafael-aero <8791811+rafael-aero@users.noreply.github.com>
2019-06-29 17:15:02 +0200

    remove unused code

--- Src/HNSW.Net.Demo/Program.cs
+++ Src/HNSW.Net.Demo/Program.cs
@@ -109,112 +109,11 @@ namespace HNSW.Net.Demo
             {
                 var vector = new float[vectorSize];
                 DefaultRandomGenerator.Instance.NextFloats(vector);
-                Normalize(ref vector);
+                VectorUtils.NormalizeSIMD(vector);
                 vectors.Add(vector);
             }
 
             return vectors;
-        }
-
-        public static void Normalize(ref float[] vector)
-        {
-            var f = DotProduct(ref vector, ref vector);
-            float f1 = (float)(1 / Math.Sqrt(f));
-            Debug.Assert(!float.IsNaN(f1));
-            Multiply(ref vector, f1);
-        }
-
-
-        private static readonly int _vs1 = Vector<float>.Count;
-        private static readonly int _vs2 = 2 * Vector<float>.Count;
-        private static readonly int _vs3 = 3 * Vector<float>.Count;
-        private static readonly int _vs4 = 4 * Vector<float>.Count;
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float DotProduct(ref float[] lhs, ref float[] rhs)
-        {
-            float result = 0f;
-
-            var count = lhs.Length;
-            var offset = 0;
-
-            while (count >= _vs4)
-            {
-                result += Vector.Dot(new Vector<float>(lhs, offset), new Vector<float>(rhs, offset));
-                result += Vector.Dot(new Vector<float>(lhs, offset + _vs1), new Vector<float>(rhs, offset + _vs1));
-                result += Vector.Dot(new Vector<float>(lhs, offset + _vs2), new Vector<float>(rhs, offset + _vs2));
-                result += Vector.Dot(new Vector<float>(lhs, offset + _vs3), new Vector<float>(rhs, offset + _vs3));
-                if (count == _vs4) return result;
-                count -= _vs4;
-                offset += _vs4;
-            }
-
-            if (count >= _vs2)
-            {
-                result += Vector.Dot(new Vector<float>(lhs, offset), new Vector<float>(rhs, offset));
-                result += Vector.Dot(new Vector<float>(lhs, offset + _vs1), new Vector<float>(rhs, offset + _vs1));
-                if (count == _vs2) return result;
-                count -= _vs2;
-                offset += _vs2;
-            }
-            if (count >= _vs1)
-            {
-                result += Vector.Dot(new Vector<float>(lhs, offset), new Vector<float>(rhs, offset));
-                if (count == _vs1) return result;
-                count -= _vs1;
-                offset += _vs1;
-            }
-            if (count > 0)
-            {
-                while (count > 0)
-                {
-                    result += lhs[offset] * rhs[offset];
-                    offset++; count--;
-                }
-            }
-            return result;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void Multiply(ref float[] lhs, float f)
-        {
-            var count = lhs.Length;
-            var offset = 0;
-
-            while (count >= _vs4)
-            {
-                (new Vector<float>(lhs, offset) * f).CopyTo(lhs, offset);
-                (new Vector<float>(lhs, offset + _vs1) * f).CopyTo(lhs, offset + _vs1);
-                (new Vector<float>(lhs, offset + _vs2) * f).CopyTo(lhs, offset + _vs2);
-                (new Vector<float>(lhs, offset + _vs3) * f).CopyTo(lhs, offset + _vs3);
-                if (count == _vs4) return;
-                count -= _vs4;
-                offset += _vs4;
-            }
-
-            if (count >= _vs2)
-            {
-                (new Vector<float>(lhs, offset) * f).CopyTo(lhs, offset);
-                (new Vector<float>(lhs, offset + _vs1) * f).CopyTo(lhs, offset + _vs1);
-                if (count == _vs2) return;
-                count -= _vs2;
-                offset += _vs2;
-            }
-            if (count >= _vs1)
-            {
-                (new Vector<float>(lhs, offset) * f).CopyTo(lhs, offset);
-                if (count == _vs1) return;
-                count -= _vs1;
-                offset += _vs1;
-            }
-            if (count > 0)
-            {
-                while (count > 0)
-                {
-                    lhs[offset] *= f;
-                    offset++; count--;
-                }
-            }
         }
 
         private class MetricsEventListener : EventListener
